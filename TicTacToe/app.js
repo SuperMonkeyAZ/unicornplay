76058ec7478bc68d0a50554ea3461a71a4a1412d
Andrew Zhao <andzhao@outlook.com>
2020-08-11 17:33:24 -0700

    Made basic idea of game

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -3,11 +3,16 @@ let who_is_next = 1
 
 console.log("test")
 
-function change(id){
-    console.log(id)
+function change(element, index) {
+    if (board[index] !== 0){
+        return;
+    }
+    board[index] = who_is_next
     if (who_is_next === 1) {
         who_is_next = 2
+        element.innerHTML = "<img src = \"circle.png\">"
     }else if (who_is_next === 2){
         who_is_next = 1
+        element.innerHTML = "<img src = \"cross.png\">"
     }
 }
