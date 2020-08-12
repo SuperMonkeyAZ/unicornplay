ea69170650bdaef9dd59f60c8e08444a55d66b45
Andrew Zhao <andzhao@outlook.com>
2020-08-11 20:50:19 -0700

    Added player turn

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -1,7 +1,9 @@
 let board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 let who_is_next = 1
+document.getElementById("demo").innerHTML = "Player 1\'s turn";
 
-console.log("test")
+
+
 
 function change(element, index) {
     if (board[index] !== 0){
@@ -10,9 +12,11 @@ function change(element, index) {
     board[index] = who_is_next
     if (who_is_next === 1) {
         who_is_next = 2
+        document.getElementById("demo").innerHTML = "Player 2\'s turn";
         element.innerHTML = "<img src = \"circle.png\">"
     }else if (who_is_next === 2){
         who_is_next = 1
+        document.getElementById("demo").innerHTML = "Player 1\'s turn";
         element.innerHTML = "<img src = \"cross.png\">"
     }
 }
